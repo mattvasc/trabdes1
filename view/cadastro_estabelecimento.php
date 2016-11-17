462c82b9c7e32c3cd610e5d4e4bd8787f112a8df
mattvasc <mateus.s.vasconcelos@outlook.com>
2016-11-17 21:23:07 -0200

    mahoe

---
+++ view/cadastro_estabelecimento.php
@@ -0,0 +1,36 @@
+<html>
+  <head>
+    <meta content="text/html; charset=utf-8" http-equiv="content-type">
+  </head>
+  <body>
+    <center>
+      <h1> Cadastro do Estabelecimento </h1>
+      <form method="get">
+        <table>
+          <tbody>
+            <tr>
+              <td><label> CNPJ: </label></td>
+              <td><input name="cnpj" maxlength="14" type="text"> <br>
+              </td>
+            </tr>
+            <tr>
+              <td><label> Razão Social: </label></td>
+              <td><input name="razao_social" type="text"> </td>
+            </tr>
+            <tr>
+              <td><label> Nome Fantasia: </label></td>
+              <td><input name="nome_fantasia" type="text"> </td>
+            </tr>
+            <tr>
+              <td><label> Nome Fantasia:</label></td>
+              <td> <input name="nome_fantasia" type="text"> </td>
+            </tr>
+          </tbody>
+        </table>
+        <input value="Adicionar Responsável" type="button"><br>
+              <input value="Adicionar Categoria" type="button"><br>
+              <input value="Adicionar Localização" type="button"><br>
+              <input value="Salvar" type="submit">
+    </form>
+  </center>
+</body></html>
