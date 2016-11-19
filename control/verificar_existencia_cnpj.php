0cd98cc1d3d70d4048bee8f639fd30036c82eb88
mattvasc <mateus.s.vasconcelos@outlook.com>
2016-11-19 19:59:43 -0200

    Mais um controller para a gente adicionar na documentacao

---
+++ control/verificar_existencia_cnpj.php
@@ -0,0 +1,15 @@
+<?php
+  if(isset($_POST['cnpj']))
+  {
+    require_once('Connection.php');
+    $conn = Connection::open();
+    $query = "SELECT * FROM `estabelecimento` WHERE `cnpj` = '".$_POST['cnpj']."';";
+    $result = mysqli_query($conn, $query);
+    if(mysqli_fetch_assoc($result))
+      echo json_encode("1");
+    else
+      echo json_encode("0");
+    mysqli_free_result($result);
+    Connection::closeConnection($conn);
+  }
+?>
