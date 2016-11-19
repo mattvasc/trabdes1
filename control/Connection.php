ee6f94c4242feb3a90fb70b97085cbec5ed3c523
mattvasc <mateus.s.vasconcelos@outlook.com>
2016-11-19 20:00:29 -0200

    Adicionei ut8 na conexão, para suportar acentos

--- control/Connection.php
+++ control/Connection.php
@@ -1,23 +1,17 @@
 <?php
-
 final class Connection{
 
   public static function open(){
       $conn = mysqli_connect('localhost:3306', 'root','','2101_airlines');
-
+      mysqli_set_charset($conn, "utf8");
       if (!$conn) {
          $conn=null;
       }
-
       return $conn;
-
-
   }
 
   public static function closeConnection($conn){
-
              mysqli_close($conn);
   }
 }
-
 ?>
