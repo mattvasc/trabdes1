3eb830a0bbfe114181119a86e120db87a0709535
mattvasc <mateus.s.vasconcelos@outlook.com>
2016-11-18 22:28:45 -0200

    Ficando ate tarde na ufscar para arrumar isso, \o/

--- control/cadastro_estabelecimento.php
+++ control/cadastro_estabelecimento.php
@@ -1,28 +1,91 @@
 <?php
   require_once('Connection.php');
   $conn = Connection::open();
+  ?>
+  <script type="text/javascript">
+      window.onload = function () {
+  <?php
   $query = "SELECT `setor` FROM `local` GROUP BY `setor` ORDER BY `setor`" ;
 
     if ($result = mysqli_query($conn, $query)) {
     ?>
-    <script type="text/javascript">
-      window.onload = function () {
-      var select = document.getElementById("setor");
-      var option;
+
+    var select = document.getElementById("setor");
+    var option;
+  <?php
+  while ($row = mysqli_fetch_assoc($result)) { ?>
+
+      option = document.createElement('option');
+      option.text = option.value = '<?php echo $row["setor"];?>';
+      select.add(option, select.length);
     <?php
-    while ($row = mysqli_fetch_assoc($result)) { ?>
+  }
 
-        option = document.createElement('option');
-        option.text = option.value = '<?php echo $row["setor"];?>';
-        select.add(option, select.length);
-      <?php
-    }
-  ?>
-};
-
-</script>
-<?php
 }
   mysqli_free_result($result);
-  mysqli_close($conn);
-?>
+  $query = "SELECT `nome` FROM `categoria`;";
+  if ($result = mysqli_query($conn, $query)) {?>
+    var container  = document.getElementById("categoria_tbl");
+    var checkbox;
+    var label;
+    var row; var column;
+    <?php
+      $i = 0;
+      while ($row = mysqli_fetch_assoc($result)) {
+         ?>
+        checkbox = document.createElement('input');
+        checkbox.type = "checkbox";
+        checkbox.name = "chkBx";
+        checkbox.value = "<?php echo $row['nome'];?>";
+        checkbox.id = checkbox.value;
+
+        label = document.createElement('label')
+        label.htmlFor = checkbox.value;
+        label.appendChild(document.createTextNode('<?php echo utf8_encode($row['nome']);?>'));
+        <?php if($i%2==0) {
+        ?>
+          row = document.createElement('div');
+          row.class = ('form-row');
+        <?php
+        }
+      ?>
+        column = document.createElement('div');
+        column.class = ('form-column');
+        column.appendChild(checkbox);
+        column.appendChild(label);
+        row.appendChild(column);
+
+        <?php
+        if($i%2==1){
+            ?>
+            container.appendChild(row);
+            <?php
+        }?>
+        <?php
+        $i++;
+      }
+      if($i%2==1){
+          ?>
+          container.appendChild(row);
+          <?php
+        $i=0;
+      }
+
+  }
+  Connection::closeConnection($conn);
+    ?>
+    /*$('input[type="checkbox"][name="chkBx"]').on('change',function(){
+      var getArrVal = $('input[type="checkbox"][name="chkBx"]:checked').map(function(){
+        return this.value;
+      }).toArray();
+      if(getArrVal.length){
+      //execute the code
+      $('#cont').html(getArrVal.toString());
+      } else {
+        $(this).prop("checked",true);
+        return false;
+      };
+    });*/
+    };
+  </script>
+<?php
--- view/cadastro_estabelecimento.php
+++ view/cadastro_estabelecimento.php
@@ -1,210 +1,20 @@
 <!DOCTYPE html>
-<html>
-<head>
-
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<script src="assets/jquery-3.1.1.js"></script>
-	<script src="assets/jquery.mask.js"></script>
-	<link rel="stylesheet" href="assets/demo.css">
-	<link rel="stylesheet" href="assets/form-validation.css">
-	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
-
-	<title>Cadastro</title>
-	<script type="text/javascript">
-		function validar(){
-			if(validar_cnpj() && validar_string('razao_social') && validar_string('nome_fantasia') && validar_string_op('site') && validar_telefone())
-			{
-				document.getElementById("myform").submit();
-			}
-			else {
-				alert("Dados inválidos! Revise o formulário e tente novamente");
-			}
-		}
-		function validar_telefone()
-		{
-			var Field = $('.form-input-telefone-row');
-			Field.removeClass('form-invalid-data');
-			Field.removeClass('form-valid-data');
-			var value = $('#telefone').cleanVal();
-			if(value.length != 10 && value.length != 11 && value.length != 0)
-			{
-				Field.addClass('form-invalid-data');
-				Field.find('.form-invalid-data-info').text('Informe um telefone Válido!');
-				return false;
-			}
-			else if(value.length != 0){
-				Field.addClass('form-valid-data');
-				return true;
-			}
-			return true;
-		}
-
-		function validar_string(id)
-		{
-				var str1 = '.form-input-';
-				var str2 = id;
-				var str3 = '-row';
-				var Field = $(str1.concat(str2,str3));
-				Field.removeClass('form-invalid-data');
-				Field.removeClass('form-valid-data');
-				var str4 = document.getElementById(id).value;
-				if(str4.length>3)
-				{
-					Field.addClass('form-valid-data');
-					return 1;
-				}
-				else if(str4.length==0)
-				{
-						Field.addClass('form-invalid-data');
-						Field.find('.form-invalid-data-info').text('Campo Obrigatório!');
-						return 0;
-				}
-				else
-				{
-						Field.addClass('form-invalid-data');
-						Field.find('.form-invalid-data-info').text('Valor muito curto!');
-						return 0;
-				}
-		}
-		function validar_string_op(id)
-		{
-				var str1 = '.form-input-';
-				var str2 = id;
-				var str3 = '-row';
-				var Field = $(str1.concat(str2,str3));
-				Field.removeClass('form-invalid-data');
-				Field.removeClass('form-valid-data');
-				var str4 = document.getElementById(id).value;
-				if(str4.length>3)
-				{
-					Field.addClass('form-valid-data');
-					return 1;
-				}
-			return 1;
-		}
-		function validar_cnpj()
-		{
-			var cnpj = $('#cnpj').cleanVal();
-			var Field = $('.form-input-cnpj-row');
-			Field.removeClass('form-invalid-data');
-			Field.removeClass('form-valid-data');
-			if(cnpj.length!=14){
-				Field.addClass('form-invalid-data');
-				Field.find('.form-invalid-data-info').text('Campo obrigatório!');
-				return 0;
-			}
-			var resto;
-			var primeiro_digito;
-			var segundo_digito;
-			var somatorio = 0;
-			somatorio+= 5 * parseInt(cnpj[0]);
-			somatorio+= 4 * parseInt(cnpj[1]);
-			somatorio+= 3 * parseInt(cnpj[2]);
-			somatorio+= 2 * parseInt(cnpj[3]);
-			somatorio+= 9 * parseInt(cnpj[4]);
-			somatorio+= 8 * parseInt(cnpj[5]);
-			somatorio+= 7 * parseInt(cnpj[6]);
-			somatorio+= 6 * parseInt(cnpj[7]);
-			somatorio+= 5 * parseInt(cnpj[8]);
-			somatorio+= 4 * parseInt(cnpj[9]);
-			somatorio+= 3 * parseInt(cnpj[10]);
-			somatorio+= 2 * parseInt(cnpj[11]);
-			resto = somatorio%11;
-			if(resto<2)
-				primeiro_digito = 0;
-			else
-				primeiro_digito = 11-resto;
-			if(parseInt(cnpj[12])!=primeiro_digito){
-				Field.addClass('form-invalid-data');
-				Field.find('.form-invalid-data-info').text('Informe um CNPJ Válido!');
-				return 0;
-			}
-			somatorio = 0;
-			somatorio += 6 * parseInt(cnpj[0]);
-			somatorio += 5 * parseInt(cnpj[1]);
-			somatorio += 4 * parseInt(cnpj[2]);
-			somatorio += 3 * parseInt(cnpj[3]);
-			somatorio += 2 * parseInt(cnpj[4]);
-			somatorio += 9 * parseInt(cnpj[5]);
-			somatorio += 8 * parseInt(cnpj[6]);
-			somatorio += 7 * parseInt(cnpj[7]);
-			somatorio += 6 * parseInt(cnpj[8]);
-			somatorio += 5 * parseInt(cnpj[9]);
-			somatorio += 4 * parseInt(cnpj[10]);
-			somatorio += 3 * parseInt(cnpj[11]);
-			somatorio += 2 * parseInt(cnpj[12]);
-			resto = somatorio%11;
-			if(resto<2)
-				segundo_digito = 0;
-			else
-				segundo_digito = 11-resto;
-			if(parseInt(cnpj[13])!=segundo_digito){
-				Field.addClass('form-invalid-data');
-				Field.find('.form-invalid-data-info').text('Informe um CNPJ Válido!');
-				return 0;
-			}
-			Field.addClass('form-valid-data');
-			return 1;
-		};
-		$(document).ready(function(){
-			//A linha a seguir é para a mascára do telfone:
-			var SPMaskBehavior = function (val) {return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';	},spOptions = {onKeyPress: function(val, e, field, options) {field.mask(SPMaskBehavior.apply({}, arguments), options);}};
-			$('#cnpj').mask('00.000.000/0000-00', {reverse: false});
-			$('#telefone').mask(SPMaskBehavior, spOptions);
-			/*
-			 var errorField = $('.form-input-site-row');
-			 Field.addClass('form-invalid-data');
-			Field.find('.form-invalid-data-info').text('Informe um site Válido!');
-			 errorField.removeClass('form-invalid-data');
-			 var successField = $('.form-input-site-row');
- 			 successField.addClass('form-valid-data');
- 			 sucessField.addClass('form-invalid-data');
-			 */
-		});
-	</script>
-
-	<script>
-		function SubSetorizar() {
-
-			var select = document.getElementById("SubSetor");
-			var option;
-
-			while (select.length > 1) {
-				select.remove(select.length-1);
-			}
-
-			$.ajax({
-        url: '../control/setor.php',
-        type: "POST",
-        data: { Setor: $('#setor').val() },
-        //dataType: 'application/json; charset=utf-8',
-        success: function (result) {
-					result = JSON.parse(result);
-
-						//alert(data);
-            for (var x = 0; x < result.length; x++) {
-							//alert(result[x]);
-							option = document.createElement('option');
-			        option.text = option.value = result[x];
-			        select.add(option, select.length);
-						}
-        }
-    });
-		}
-
-	</script>
-
-	<link rel="stylesheet" href="assets/demo.css">
-	<link rel="stylesheet" href="assets/form-validation.css">
-
+<html lang="pt-br">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<script src="assets/js/jquery-3.1.1.js"></script>
+		<script src="assets/js/jquery.mask.js"></script>
+		<link rel="stylesheet" href="assets/css/demo.css">
+		<link rel="stylesheet" href="assets/css/form-validation.css">
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+		<title>Cadastro</title>
 </head>
-
-
+<body>
 	<header>
 		<h1><center>2101 Airlines</center></h1>
-    </header>
-
+	</header>
     <div class="main-content">
 
         <!-- You only need this form and the form-validation.css -->
@@ -273,62 +83,40 @@
                 <label>
                     <span>Setor</span>
                     <select name="setor" id="setor" onchange="SubSetorizar();">
-                        <option>Escolha...</option>
+                        <option value="">Escolha...</option>
                     </select>
                 </label>
-
+								<span class="form-valid-data-sign"><i class="fa fa-check"></i></span>
+		            <span class="form-invalid-data-sign"><i class="fa fa-close"></i></span>
+		            <span class="form-invalid-data-info"></span>
             </div>
-            <div class="form-row form-input-local-subsetor">
+            <div class="form-row form-input-subsetor-row">
 
                 <label>
                     <span>Subsetor</span>
                     <select name="subsetor" id="SubSetor">
-                        <option>Escolha...</option>
+                        <option value="">Escolha...</option>
                     </select>
                 </label>
-
+								<span class="form-valid-data-sign"><i class="fa fa-check"></i></span>
+		            <span class="form-invalid-data-sign"><i class="fa fa-close"></i></span>
+		            <span class="form-invalid-data-info"></span>
             </div>
-
-            <div class="form-row form-input-categoria">
+            <div  align="center" class="form-row form-input-categoria-row" >
 
                 <label class="form-checkbox">
                   <!--TAB é no CSS -->
-                    <span>Categoria</span><br>
-                    <table>
-                      <tr>
-                        <td><input type="checkbox" name="checkbox_aluguel_carros" value="Aluguel de Carros">Aluguel de Carros</td>
-                        <td><input type="checkbox" name="checkbox_fastfood" value="Fast Food"> Fast Food</td>
-                      </tr>
-                      <tr>
-                        <td><input type="checkbox" name="checkbox_banco" value="Banco"> Banco</td>
-                        <td><input type="checkbox" name="checkbox_livraria" value="Livraria"> Livraria</td>
-                      </tr>
-                      <tr>
-                        <td><input type="checkbox" name="checkbox_bar" value="Bar"> Bar</td>
-                        <td><input type="checkbox" name="checkbox_orgao" value="Órgão Públicos"> Órgão Público</td>
-                      </tr>
-                      <tr>
-                        <td><input type="checkbox" name="checkbox_cafe" value="Café"> Café</td>
-                        <td><input type="checkbox" name="checkbox_moda" value="Moda/Presentes"> Moda/Presentes</td>
-                      </tr>
-                      <tr>
-                        <td><input type="checkbox" name="checkbox_casa_cambio" value="Casa de Câmbio"> Casa de Câmbio</td>
-                        <td><input type="checkbox" name="checkbox_perfumaria" value="Perfumaria"> Perfumaria</td>
-                      </tr>
-                      <tr>
-                        <td><input type="checkbox" name="checkbox_doce" value="Doces e Sorvetes"> Doces e Sorvetes</td>
-                        <td><input type="checkbox" name="checkbox_restaurante" value="Restaurante"> Restaurante</td>
-                      </tr>
-                      <tr>
-                        <td><input type="checkbox" name="checkbox_drogaria" value="Drogaria"> Drogaria </td>
-                        <td><input type="checkbox" name="checkbox_transporte_carga" value="Transporte de Carga"> Transporte de Carga</td>
-                      </tr>
-                    </table>
+
+                    <span>Categoria</span><br><br>
+                    <div name="categoria_tbl" id="categoria_tbl" align="left">
+											<input type='checkbox' name="gambiarra" id="gambiarra" value="" style="display:none">
+                    </div>
 
                 </label>
-
+								<span class="form-valid-data-sign"><i class="fa fa-check"></i></span>
+		            <span class="form-invalid-data-sign"><i class="fa fa-close"></i></span>
+		            <span class="form-invalid-data-info"></span>
             </div>
-
             <div class="form-row ">
 
                 <button type="button" onclick="validar()">Próximo</button>
@@ -338,11 +126,223 @@
         </form>
 
     </div>
+		<footer>
+			<script type="text/javascript">
+				function validar(){
+					var resposta = true;
+					resposta = resposta && validar_cnpj();
+					resposta = validar_string('razao_social') && resposta;
+					resposta = validar_string('nome_fantasia') && resposta;
+					resposta = validar_string_op('site') && resposta;
+					resposta = validar_telefone() && resposta;
+					resposta = validar_setor(1) && resposta;
+					resposta = validar_setor(0) && resposta;
+					resposta = validar_categoria() && resposta;
 
+					if(resposta)
+					{
+						document.getElementById("myform").submit();
+					}
+				}
+				function validar_telefone()
+				{
+					var Field = $('.form-input-telefone-row');
+					Field.removeClass('form-invalid-data');
+					Field.removeClass('form-valid-data');
+					var value = $('#telefone').cleanVal();
+					if(value.length != 10 && value.length != 11 && value.length != 0)
+					{
+						Field.addClass('form-invalid-data');
+						Field.find('.form-invalid-data-info').text('Informe um telefone Válido!');
+						return false;
+					}
+					else if(value.length != 0){
+						Field.addClass('form-valid-data');
+						return true;
+					}
+					return true;
+				}
+
+				function validar_string(id)
+				{
+						var str1 = '.form-input-';
+						var str2 = id;
+						var str3 = '-row';
+						var Field = $(str1.concat(str2,str3));
+						Field.removeClass('form-invalid-data');
+						Field.removeClass('form-valid-data');
+						var str4 = document.getElementById(id).value;
+						if(str4.length>3)
+						{
+							Field.addClass('form-valid-data');
+							return 1;
+						}
+						else if(str4.length==0)
+						{
+								Field.addClass('form-invalid-data');
+								Field.find('.form-invalid-data-info').text('Campo Obrigatório!');
+								return 0;
+						}
+						else
+						{
+								Field.addClass('form-invalid-data');
+								Field.find('.form-invalid-data-info').text('Valor muito curto!');
+								return 0;
+						}
+				}
+				function validar_string_op(id)
+				{
+						var str1 = '.form-input-';
+						var str2 = id;
+						var str3 = '-row';
+						var Field = $(str1.concat(str2,str3));
+						Field.removeClass('form-invalid-data');
+						Field.removeClass('form-valid-data');
+						var str4 = document.getElementById(id).value;
+						if(str4.length>3)
+						{
+							Field.addClass('form-valid-data');
+							return 1;
+						}
+					return 1;
+				}
+				function validar_setor(subsetor){
+						var selecionado; var classe;
+						if(!subsetor){
+						 	selecionado = $("#SubSetor").val();
+							classe = $('.form-input-subsetor-row');
+						}
+						else{
+							selecionado = $('#setor').val();
+							classe = $('.form-input-setor-row');
+						}
+						classe.removeClass('form-invalid-data');
+						classe.removeClass('form-valid-data');
+						if(selecionado)
+							return 1;
+						classe.addClass('form-invalid-data');
+						classe.find('.form-invalid-data-info').text('Campo obrigatório!');
+						return 0;
+				}
+				function validar_categoria()
+				{
+					var classe = $('.form-input-categoria-row');
+					classe.removeClass('form-invalid-data');
+					classe.removeClass('form-valid-data');
+					var getArrVal = $('input[type="checkbox"][name="chkBx"]:checked').map(function(){
+		        return this.value;
+		      }).toArray();
+		      if(!getArrVal.length){
+						classe.addClass('form-invalid-data');
+						classe.find('.form-invalid-data-info').text('Selecione ao menos uma!');
+						return 0;
+					}
+					return 1;
+				}
+				function validar_cnpj()
+				{
+					var cnpj = $('#cnpj').cleanVal();
+					var Field = $('.form-input-cnpj-row');
+					Field.removeClass('form-invalid-data');
+					Field.removeClass('form-valid-data');
+					if(cnpj.length!=14){
+						Field.addClass('form-invalid-data');
+						Field.find('.form-invalid-data-info').text('Campo obrigatório!');
+						return 0;
+					}
+					var resto;
+					var primeiro_digito;
+					var segundo_digito;
+					var somatorio = 0;
+					somatorio+= 5 * parseInt(cnpj[0]);
+					somatorio+= 4 * parseInt(cnpj[1]);
+					somatorio+= 3 * parseInt(cnpj[2]);
+					somatorio+= 2 * parseInt(cnpj[3]);
+					somatorio+= 9 * parseInt(cnpj[4]);
+					somatorio+= 8 * parseInt(cnpj[5]);
+					somatorio+= 7 * parseInt(cnpj[6]);
+					somatorio+= 6 * parseInt(cnpj[7]);
+					somatorio+= 5 * parseInt(cnpj[8]);
+					somatorio+= 4 * parseInt(cnpj[9]);
+					somatorio+= 3 * parseInt(cnpj[10]);
+					somatorio+= 2 * parseInt(cnpj[11]);
+					resto = somatorio%11;
+					if(resto<2)
+						primeiro_digito = 0;
+					else
+						primeiro_digito = 11-resto;
+					if(parseInt(cnpj[12])!=primeiro_digito){
+						Field.addClass('form-invalid-data');
+						Field.find('.form-invalid-data-info').text('Informe um CNPJ Válido!');
+						return 0;
+					}
+					somatorio = 0;
+					somatorio += 6 * parseInt(cnpj[0]);
+					somatorio += 5 * parseInt(cnpj[1]);
+					somatorio += 4 * parseInt(cnpj[2]);
+					somatorio += 3 * parseInt(cnpj[3]);
+					somatorio += 2 * parseInt(cnpj[4]);
+					somatorio += 9 * parseInt(cnpj[5]);
+					somatorio += 8 * parseInt(cnpj[6]);
+					somatorio += 7 * parseInt(cnpj[7]);
+					somatorio += 6 * parseInt(cnpj[8]);
+					somatorio += 5 * parseInt(cnpj[9]);
+					somatorio += 4 * parseInt(cnpj[10]);
+					somatorio += 3 * parseInt(cnpj[11]);
+					somatorio += 2 * parseInt(cnpj[12]);
+					resto = somatorio%11;
+					if(resto<2)
+						segundo_digito = 0;
+					else
+						segundo_digito = 11-resto;
+					if(parseInt(cnpj[13])!=segundo_digito){
+						Field.addClass('form-invalid-data');
+						Field.find('.form-invalid-data-info').text('Informe um CNPJ Válido!');
+						return 0;
+					}
+					Field.addClass('form-valid-data');
+					return 1;
+				};
+				$(document).ready(function(){
+					//A linha a seguir é para a mascára do telfone:
+					var SPMaskBehavior = function (val) {return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';	},spOptions = {onKeyPress: function(val, e, field, options) {field.mask(SPMaskBehavior.apply({}, arguments), options);}};
+					$('#cnpj').mask('00.000.000/0000-00', {reverse: false});
+					$('#telefone').mask(SPMaskBehavior, spOptions);
+				});
+				function SubSetorizar() {
+
+					var select = document.getElementById("SubSetor");
+					var option;
+
+					while (select.length > 1) {
+						select.remove(select.length-1);
+					}
+
+					$.ajax({
+		        url: '../control/setor.php',
+		        type: "POST",
+		        data: { Setor: $('#setor').val() },
+		        //dataType: 'application/json; charset=utf-8',
+		        success: function (result) {
+							result = JSON.parse(result);
+
+								//alert(data);
+		            for (var x = 0; x < result.length; x++) {
+									//alert(result[x]);
+									option = document.createElement('option');
+					        option.text = option.value = result[x];
+					        select.add(option, select.length);
+								}
+		        }
+		    });
+				}
+			</script>
+			<?php
+				 require_once('../control/cadastro_estabelecimento.php');
+			?>
+		</footer>
 </body>
 
-<?php
-	 require_once('../control/cadastro_estabelecimento.php');
-?>
+
 
 </html>
