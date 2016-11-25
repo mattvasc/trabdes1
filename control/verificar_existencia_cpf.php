7dcb39a9b515175c05c53275d6c773e5a090008d
Carla Gama <carla.gaama@dcomp.sor.ufscar.br>
2016-11-24 23:10:28 -0200

    preciso do banco pra checar

--- control/verificar_existencia_cpf.php
+++ control/verificar_existencia_cpf.php
@@ -1,15 +1,15 @@
 <?php
-  if(isset($_POST['cpf']) && isset($_POST['cnpj']))
+  if(isset($_POST['cpf']) && isset($_POST['estabelecimento']))
   {
     require_once('Connection.php');
     $conn = Connection::open();
-    $query = "SELECT * FROM `estabelecimento` NATURAL JOIN 'responsavel' WHERE 'cnpj' = '"+ .$_POST['cnpj']."';";
+    $query = "SELECT * FROM `estabelecimento` NATURAL JOIN 'responsavel' WHERE 'cnpj' = '"+ .$_POST['estabelecimento']->getCnpj();."';";
     $result = mysqli_query($conn, $query);
     if(mysqli_fetch_assoc($result))
       echo json_encode("1");
     else
       echo json_encode("0");
-      
+
     mysqli_free_result($result);
     Connection::closeConnection($conn);
   }
