7977168517045e7a302799e3aa07fe7e9e245cfa
mattvasc <mateus.s.vasconcelos@outlook.com>
2016-11-27 18:05:22 -0200

    Nova classe para busca adicionada

---
+++ model/resultado.class.php
@@ -0,0 +1,34 @@
+<?php
+  class Resultado{
+    private $campo_pesquisado;
+    private $valor_campo;
+    private $estabelecimento;
+
+      public function __construct()
+      {
+        $this->estabelecimento = array();
+      }
+      public function addEstabelecimento($estabelecimento){
+        $this->estabelecimento[] = $estabelecimento;
+      }
+
+      public function setCampoPesquisado($campo){ /*'CNPJ' ou 'Nome Fantasia' ou 'Razão Social e etc...'*/
+        $this->campo_pesquisado = $campo;
+      }
+      public function setValorCampo($valor){ /*123123123123 ou 'Mec Dolands' ou 'Restaurante da dona Maria' e etc*/
+        $this->valor = $valor;
+      }
+      public function getCampoPesquisado(){
+        return $this->campo_pesquisado;
+      }
+      public function getValorCampo(){
+        return $this->valor_campo;
+      }
+      public function getEstabelecimento(){
+        return $this->estabelecimento;
+      }
+  }
+
+
+
+?>
